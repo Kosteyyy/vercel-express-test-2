7d1562227c626918e52ccac7c398591021433867
Kosteyyy <averkiev@yandex.ru>
2022-08-05 13:18:08 +0300

    req handler

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
-const express = require("express");
-const port = process.env.PORT || 5000;
+// const express = require("express");
+// const port = process.env.PORT || 5000;
 
-const app = express();
+// const app = express();
+// export default
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+// app.get("/", (req, res) => {
+//   res.send("Express on Vercel");
+// });
 
-app.listen(port, () => console.log("Running on port 5000"));
+// app.listen(port, () => console.log("Running on port 5000"));
+
+export default function handler(req, res) {
+  const { name = "World" } = req.query;
+  return res.send(`Hello ${name}!`);
+}
